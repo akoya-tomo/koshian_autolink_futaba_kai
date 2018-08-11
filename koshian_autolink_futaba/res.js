86689844351b3f1380a342310123132de2cdbbab
akoya-tomo <akoya_tomo-00755@yahoo.co.jp>
2018-08-11 09:14:43 +0900

    YouTubeのプレビュー横幅の設定を追加(#1)

--- koshian_autolink_futaba/res.js
+++ koshian_autolink_futaba/res.js
@@ -34,6 +34,7 @@ let g_hide_preview = false;
 let g_volume = 0.5;
 let max_width = 500;
 let max_height = 500;
+let youtube_width = 0;
 let sio_quote_link = false;
 let use_preview_link = false;
 
@@ -132,6 +133,13 @@ function replaceText(node) {
                 iframe.style.maxWidth = `${max_width}px`;
                 iframe.style.maxHeight = `${max_height}px`;
                 iframe.style.display = initial_hide ? "none" : "block";
+                if (youtube_width > 0) {
+                    let youtube_height = Math.round(youtube_width * 9 / 16);
+                    iframe.style.maxWidth = `${youtube_width}px`;
+                    iframe.style.width = `${youtube_width}px`;
+                    iframe.style.maxHeight = `${youtube_height}px`;
+                    iframe.style.height = `${youtube_height}px`;
+                }
                 parent.insertBefore(preview_switch, elem3);
                 parent.insertBefore(iframe, node);
                 parent.removeChild(node);
@@ -290,6 +298,7 @@ function onLoadSetting(result) {
     max_width = safeGetValue(result.max_width, 500);
     max_height = safeGetValue(result.max_height, 500);
     g_volume = safeGetValue(result.g_volume, 0.5);
+    youtube_width = safeGetValue(result.youtube_width, 0);
     sio_quote_link = safeGetValue(result.sio_quote_link, false);
     use_preview_link = safeGetValue(result.use_preview_link, false);
 
